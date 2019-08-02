bcf84afbb8ef41398d5485d14104b4eb5cf3c757
niniack <nsa325@nyu.edu>
2019-08-02 18:21:33 -0400

    rotate using numpy-stl instead of matplotlib view angles

--- src/dmxread.py
+++ src/dmxread.py
@@ -7,9 +7,12 @@ from stl import mesh
 from mpl_toolkits import mplot3d
 from matplotlib import pyplot
 
+import math
+
 from pylibdmtx.pylibdmtx import decode
 import cv2
 
+offset = [37,55]
 
 # Create a new plot
 figure = pyplot.figure()
@@ -17,6 +20,11 @@ axes = mplot3d.Axes3D(figure, proj_type = 'ortho')
 
 # Load the STL files and add the vectors to the plot
 your_mesh = mesh.Mesh.from_file('../stl/extractedCode.stl')
+
+your_mesh.rotate([0,0,1],math.radians(-offset[1]))
+your_mesh.rotate([1,0,0],math.radians(-offset[0]))
+
+
 axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors, facecolors='black', edgecolors='black'))
 
 # Auto scale to the mesh size
@@ -24,7 +32,7 @@ scale = your_mesh.points.flatten(-1)
 axes.auto_scale_xyz(scale, scale, scale)
 # axes.autoscale()
 
-axes.view_init(elev=55, azim=37)
+axes.view_init(elev=90, azim=0)
 
 # Show the plot to the screen
 # pyplot.show(figure)
